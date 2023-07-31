0533e5292738c0f8a3afa95573bf38f8500d7acf
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-01 00:00:00 +0100

    Add ESLint configuration and fix linting issues

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
-import { Text, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular as Roboto400Regular,
+  Roboto_700Bold as Roboto700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
-  let [fontsLoaded, fontError] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-  });
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular: Roboto400Regular,
+    Roboto_700Bold: Roboto700Bold,
+  })
 
   if (!fontsLoaded) {
-    return <Text>Loading fonts...</Text>;
+    return <Text>Loading fonts...</Text>
   }
- 
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 32}}>Hello FitTrackPro</Text>
+      <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 32 }}>
+        Hello FitTrackPro
+      </Text>
     </View>
-  );
+  )
 }
