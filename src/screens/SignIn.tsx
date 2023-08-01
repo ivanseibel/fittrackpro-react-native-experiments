b1d027b5f39f56fa020132d6644b50a2c36087f9
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-02 00:00:00 +0100

    Add input field to SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export default function SignIn() {
   return (
@@ -12,16 +13,21 @@ export default function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
-      <Center my={24}>
-        <LogoSvg />
-        <Text fontSize="sm" color="gray.100">
-          Strengthen your mind and body
-        </Text>
-      </Center>
-      <Center>
-        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-          Access your account
-        </Heading>
+      <Center flex={1} justifyContent={'flex-start'} paddingX="10">
+        <Center my={24}>
+          <LogoSvg />
+          <Text fontSize="sm" color="gray.100">
+            Strengthen your mind and body
+          </Text>
+        </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Access your account
+          </Heading>
+        </Center>
+        <Center>
+          <Input placeholder="Email" />
+        </Center>
       </Center>
     </VStack>
   )
