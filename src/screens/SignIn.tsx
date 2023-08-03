2b08a76964080c8b1692313d506eb27febd44251
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-04 00:00:00 +0100

    Add logo and tagline to SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,7 @@
-import { VStack, Image } from 'native-base'
+import { VStack, Image, Text, Center } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
 
 export default function SignIn() {
   return (
@@ -11,6 +12,12 @@ export default function SignIn() {
         resizeMode="contain"
         position="absolute"
       />
+      <Center my={24}>
+        <LogoSvg />
+        <Text fontSize="sm" color="gray.100">
+          Strengthen your mind and body
+        </Text>
+      </Center>
     </VStack>
   )
 }
