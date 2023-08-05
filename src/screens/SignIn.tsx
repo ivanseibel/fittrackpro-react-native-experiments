b813f15f09f77a4f3d9b768773dcdfe4672a29b8
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-06 00:00:00 +0100

    Refactor sign-in screen layout

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -5,61 +5,58 @@ import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
 import { DismissKeyboardView } from '@components/DismissKeyboardView'
 import { Button } from '@components/Button'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SignIn() {
   return (
     <DismissKeyboardView>
-      <SafeAreaView style={{ flex: 1 }}>
-        <VStack flex={1} bg={'gray.700'} px={'10'}>
-          <Image
-            source={BackgroundImg}
-            alt=""
-            resizeMode="contain"
-            position="absolute"
-          />
-          <Center my={24}>
-            <LogoSvg />
-            <Text fontSize="sm" color="gray.100" lineHeight={'sm'}>
-              Strengthen your mind and body
-            </Text>
-          </Center>
-          <Center>
-            <Heading
-              color="gray.100"
-              fontSize="xl"
-              mb={6}
-              fontFamily="heading"
-              lineHeight={'xl'}
-            >
-              Access your account
-            </Heading>
-          </Center>
-          <VStack space={4}>
-            <Input placeholder="Email" inputMode="email" />
-            <Input placeholder="Password" secureTextEntry />
-          </VStack>
-          <Center mt={8}>
-            <Button w={'full'} h={14}>
-              Sign in
-            </Button>
-          </Center>
-          <VStack
-            flex={1}
-            justifyContent={'flex-end'}
-            mb={'10'}
-            alignItems={'center'}
-            space={4}
+      <VStack flex={1} bg={'gray.700'} px={'10'}>
+        <Image
+          source={BackgroundImg}
+          alt=""
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text fontSize="sm" color="gray.100" lineHeight={'sm'}>
+            Strengthen your mind and body
+          </Text>
+        </Center>
+        <Center>
+          <Heading
+            color="gray.100"
+            fontSize="xl"
+            mb={6}
+            fontFamily="heading"
+            lineHeight={'xl'}
           >
-            <Heading color="gray.100" fontSize="md" mt={4} lineHeight={'md'}>
-              Don&apos;t have an account?
-            </Heading>
-            <Button w={'full'} h={14} variant={'outline'}>
-              Sign up
-            </Button>
-          </VStack>
+            Access your account
+          </Heading>
+        </Center>
+        <VStack space={4}>
+          <Input placeholder="Email" inputMode="email" />
+          <Input placeholder="Password" secureTextEntry />
         </VStack>
-      </SafeAreaView>
+        <Center mt={8}>
+          <Button w={'full'} h={14}>
+            Sign in
+          </Button>
+        </Center>
+        <VStack
+          flex={1}
+          justifyContent={'flex-end'}
+          mb={'10'}
+          alignItems={'center'}
+          space={4}
+        >
+          <Heading color="gray.100" fontSize="md" mt={4} lineHeight={'md'}>
+            Don&apos;t have an account?
+          </Heading>
+          <Button w={'full'} h={14} variant={'outline'}>
+            Sign up
+          </Button>
+        </VStack>
+      </VStack>
     </DismissKeyboardView>
   )
 }
