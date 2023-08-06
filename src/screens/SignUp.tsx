79c84da522284fbe69c9a491eeafab7948007c1d
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-07 00:00:00 +0100

    Add SignUp screen component

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,72 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+
+import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
+import { Input } from '@components/Input'
+import { DismissKeyboardView } from '@components/DismissKeyboardView'
+import { Button } from '@components/Button'
+
+export default function SignUp() {
+  return (
+    <DismissKeyboardView>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <VStack flex={1} bg={'gray.700'} px={'10'}>
+          <Image
+            source={BackgroundImg}
+            alt=""
+            resizeMode="contain"
+            position="absolute"
+          />
+          <Center my={24}>
+            <LogoSvg />
+            <Text fontSize="sm" color="gray.100" lineHeight={'sm'}>
+              Strengthen your mind and body
+            </Text>
+          </Center>
+          <Center>
+            <Heading
+              color="gray.100"
+              fontSize="xl"
+              mb={6}
+              fontFamily="heading"
+              lineHeight={'xl'}
+            >
+              Create your account
+            </Heading>
+          </Center>
+          <VStack space={4}>
+            <Input placeholder="Name" inputMode="text" autoCapitalize="words" />
+            <Input
+              placeholder="Email"
+              inputMode="email"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Password" secureTextEntry />
+            <Input placeholder="Confirm password" secureTextEntry />
+          </VStack>
+          <Center mt={8}>
+            <Button w={'full'} h={14}>
+              Create account
+            </Button>
+          </Center>
+          <VStack
+            flex={1}
+            justifyContent={'flex-end'}
+            mb={'10'}
+            alignItems={'center'}
+            space={4}
+          >
+            <Button w={'full'} h={14} variant={'outline'}>
+              Back to Sign in
+            </Button>
+          </VStack>
+        </VStack>
+      </ScrollView>
+    </DismissKeyboardView>
+  )
+}
