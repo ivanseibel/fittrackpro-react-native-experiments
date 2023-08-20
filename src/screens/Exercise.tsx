61687e134ecb5e324f92e1b293a6d047bd779446
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-21 00:00:00 +0100

    Add image and button to Exercise screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,4 @@
-import { HStack, Heading, Icon, VStack, Text } from 'native-base'
+import { HStack, Heading, Icon, VStack, Text, Image, Center } from 'native-base'
 import { useNavigation } from '@react-navigation/native'
 import { TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
@@ -6,6 +6,7 @@ import { Feather } from '@expo/vector-icons'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 import BodySvg from '@assets/body.svg'
+import { Button } from '@components/Button'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -37,6 +38,7 @@ export function Exercise() {
             <Text
               color="gray.200"
               fontSize={'md'}
+              lineHeight={'md'}
               ml={1}
               textTransform="capitalize"
             >
@@ -44,6 +46,43 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack flex={1} p={8} space={3}>
+        <Image
+          source={{
+            uri: 'https://williamcarvalhoamaral.files.wordpress.com/2020/01/dorsal-blog.jpg?w=640',
+          }}
+          alt="Exercise image"
+          w={364}
+          h={364}
+          rounded={'8px'}
+          mr={4}
+          resizeMode="cover"
+        />
+
+        <VStack pt={4} pb={2} px={2} space={6}>
+          <HStack justifyContent={'center'} space={20}>
+            <Center flexDir={'row'}>
+              <Text color={'gray.200'} fontSize={'md'} lineHeight={'md'}>
+                Icon
+              </Text>
+              <Text color={'gray.200'} fontSize={'md'} lineHeight={'md'}>
+                3 series
+              </Text>
+            </Center>
+            <Center flexDir={'row'}>
+              <Text color={'gray.200'} fontSize={'md'} lineHeight={'md'}>
+                Icon
+              </Text>
+              <Text color={'gray.200'} fontSize={'md'} lineHeight={'md'}>
+                12 reps
+              </Text>
+            </Center>
+          </HStack>
+
+          <Button>Mark as completed</Button>
+        </VStack>
       </VStack>
     </VStack>
   )
