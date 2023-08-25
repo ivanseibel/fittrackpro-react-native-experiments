d369ea4f4f371ac09c9d076acf53e726a005c6b8
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-26 00:00:00 +0100

    Add password length validation to SignUp form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -22,7 +22,10 @@ const signUpSchema = yup.object().shape({
     .string()
     .email('Please enter a valid email')
     .required('Email is required'),
-  password: yup.string().required('Password is required'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'Password is too short'),
   confirmPassword: yup
     .string()
     .required('Confirm Password is required')
