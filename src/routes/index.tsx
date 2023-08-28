2ab5e602a92ddf1af3a0febc113aeed01d940cdd
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-08-29 00:00:00 +0100

    Add authentication context and update isAuthenticated value

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,14 @@ import { Box, useTheme } from 'native-base'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
+import { AuthContext } from '@contexts/AuthContext'
+import { useContext } from 'react'
 
 export function Routes() {
+  const contextData = useContext(AuthContext)
+
+  const { isAuthenticated } = contextData as { isAuthenticated: boolean }
+
   const { colors } = useTheme()
   const theme = {
     ...DefaultTheme,
@@ -13,7 +19,7 @@ export function Routes() {
     },
   }
 
-  const isAuthenticated = false
+  // const isAuthenticated = false
 
   return (
     <Box flex={1} bg="gray.700">
