853bb5b6e8de8c667f56c833c68c8c582f3a6091
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-09-03 00:00:00 +0100

    Add TOKEN_STORAGE_PREFIX constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,3 +12,4 @@ export const GENERAL_ERROR_MESSAGE =
   'An error occurred while processing your request. \nPlease try again later.'
 
 export const USER_STORAGE_PREFIX = '@FitTrackPro:user'
+export const TOKEN_STORAGE_PREFIX = '@FitTrackPro:token'
