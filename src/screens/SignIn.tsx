88933233e17d70fc81585f1f31b2b1fa8d16be6e
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-15 20:49:22 +0000

    Refactor SignIn screen component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,32 +3,35 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Input } from '@components/Input'
+import { DismissKeyboardView } from '@components/DismissKeyboardView'
 
 export default function SignIn() {
   return (
-    <VStack flex={1} bg={'gray.700'}>
-      <Image
-        source={BackgroundImg}
-        alt=""
-        resizeMode="contain"
-        position="absolute"
-      />
-      <Center flex={1} justifyContent={'flex-start'} paddingX="10">
-        <Center my={24}>
-          <LogoSvg />
-          <Text fontSize="sm" color="gray.100">
-            Strengthen your mind and body
-          </Text>
+    <DismissKeyboardView>
+      <VStack flex={1} bg={'gray.700'}>
+        <Image
+          source={BackgroundImg}
+          alt=""
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center flex={1} justifyContent={'flex-start'} paddingX={'10'}>
+          <Center my={24}>
+            <LogoSvg />
+            <Text fontSize="sm" color="gray.100">
+              Strengthen your mind and body
+            </Text>
+          </Center>
+          <Center>
+            <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+              Access your account
+            </Heading>
+          </Center>
+          <Center>
+            <Input placeholder="Email" inputMode="email" />
+          </Center>
         </Center>
-        <Center>
-          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Access your account
-          </Heading>
-        </Center>
-        <Center>
-          <Input placeholder="Email" />
-        </Center>
-      </Center>
-    </VStack>
+      </VStack>
+    </DismissKeyboardView>
   )
 }
