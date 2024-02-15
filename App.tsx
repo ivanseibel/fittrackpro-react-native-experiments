babd71adf8c6aea7c24fdc43e18f2514599b5818
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-15 23:26:05 +0000

    Replace SignUp component with Routes component in App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading'
 import { theme } from 'src/theme'
-import { SignUp } from '@screens/SignUp'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent={true}
       />
-      {!fontsLoaded ? <Loading /> : <SignUp />}
+      {!fontsLoaded ? <Loading /> : <Routes />}
     </NativeBaseProvider>
   )
 }
