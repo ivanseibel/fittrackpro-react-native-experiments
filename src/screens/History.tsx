43aa4805ad3a80c57f279945bcab6b8dc0a4e185
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 19:35:40 +0000

    Add SectionList to History screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,15 +1,89 @@
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { VStack, SectionList, Heading } from 'native-base'
+// import { SectionList } from 'react-native'
+
+type Exercise = {
+  group: string
+  name: string
+  time: string
+}
+
+type ExerciseHistorySectionList = {
+  title: string
+  data: Exercise[]
+}
+
+const exercises: ExerciseHistorySectionList[] = [
+  {
+    title: '26.08.2021',
+    data: [
+      {
+        group: 'Back',
+        name: 'Front pulldown',
+        time: '07:30',
+      },
+      {
+        group: 'Chest',
+        name: 'Bench press',
+        time: '08:30',
+      },
+    ],
+  },
+  {
+    title: '27.08.2021',
+    data: [
+      {
+        group: 'Back',
+        name: 'Front pulldown',
+        time: '07:30',
+      },
+      {
+        group: 'Chest',
+        name: 'Bench press',
+        time: '08:30',
+      },
+    ],
+  },
+  {
+    title: '28.08.2021',
+    data: [
+      {
+        group: 'Back',
+        name: 'Front pulldown',
+        time: '07:30',
+      },
+      {
+        group: 'Chest',
+        name: 'Bench press',
+        time: '08:30',
+      },
+    ],
+  },
+]
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Exercise History" />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={({ item }) => <HistoryCard {...item} />}
+        px={8}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading
+            color={'gray.200'}
+            fontSize={'md'}
+            lineHeight={'md'}
+            mt={10}
+            mb={3}
+          >
+            {title}
+          </Heading>
+        )}
+      />
     </VStack>
   )
 }
