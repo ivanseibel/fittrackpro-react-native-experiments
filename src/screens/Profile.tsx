7b00450ca84a8153c190710c0276f322eef216e9
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 19:59:32 +0000

    Add UserPhoto component to Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,20 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, ScrollView, VStack } from 'native-base'
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Profile" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            size={33}
+            alt="Ivan Seibel"
+            source={{ uri: 'https://github.com/ivanseibel.png' }}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
