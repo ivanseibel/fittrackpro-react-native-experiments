fbdc1d1b719e9abdb440ebb1ba86eb0a844737a0
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 14:20:39 +0000

    Update Home screen layout and add activity count

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { FlatList, VStack } from 'native-base'
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 import { useState } from 'react'
 
 type GroupOfExercises =
@@ -30,6 +30,7 @@ export function Home() {
         data={groups}
         keyExtractor={(group) => group}
         horizontal
+        showsHorizontalScrollIndicator={false}
         my={10}
         maxH={10}
         _contentContainerStyle={{ px: 8 }}
@@ -41,6 +42,17 @@ export function Home() {
           />
         )}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={'space-between'} mb={5}>
+          <Heading color={'gray.200'} fontSize={'md'} fontFamily={'heading'}>
+            Activities
+          </Heading>
+          <Text color={'gray.200'} fontSize={'sm'}>
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }
