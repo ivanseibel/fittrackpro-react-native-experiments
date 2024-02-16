9dd62e230d9eadf89e76724261f4d93b9e940a20
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 23:09:10 +0000

    Update HomeHeader component to center UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,4 +1,4 @@
-import { HStack, Heading, Icon, Text, VStack } from 'native-base'
+import { Center, HStack, Heading, Icon, Text, VStack } from 'native-base'
 import { MaterialIcons } from '@expo/vector-icons'
 import { UserPhoto } from './UserPhoto'
 import { TouchableOpacity } from 'react-native'
@@ -6,12 +6,13 @@ import { TouchableOpacity } from 'react-native'
 export function HomeHeader() {
   return (
     <HStack bg={'gray.600'} pt={16} pb={5} px={8} alignItems={'center'}>
-      <UserPhoto
-        size={16}
-        source={{ uri: 'https://github.com/ivanseibel.png' }}
-        alt={"User's photo"}
-        mr={4}
-      />
+      <Center mr={4}>
+        <UserPhoto
+          size={16}
+          source={{ uri: 'https://github.com/ivanseibel.png' }}
+          alt={"User's photo"}
+        />
+      </Center>
       <VStack flex={1}>
         <Text color={'gray.100'} fontSize={'md'} lineHeight={'md'}>
           Hello,
