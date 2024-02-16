e8ace54ae0e21e71cb00fe622b76403134bb0cec
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 23:09:03 +0000

    Add skeleton loading and image opacity transition to UserPhoto component

--- src/components/UserPhoto.tsx
+++ src/components/UserPhoto.tsx
@@ -1,19 +1,33 @@
-import { Center, Image, IImageProps } from 'native-base'
+import { Center, Image, IImageProps, Skeleton } from 'native-base'
+import { useState } from 'react'
 
 type UserPhotoProps = IImageProps & {
   size: number
 }
 
 export function UserPhoto({ size, alt, ...rest }: UserPhotoProps) {
+  const [imageLoaded, setImageLoaded] = useState(false)
+
   return (
-    <Center>
+    <Center position="relative">
+      <Skeleton
+        w={size}
+        h={size}
+        rounded="full"
+        startColor="gray.500"
+        endColor="gray.600"
+      />
       <Image
         w={size}
         h={size}
         alt={alt}
-        rounded={'full'}
-        borderColor={'gray.400'}
+        position={'absolute'}
+        rounded="full"
+        borderColor="gray.400"
         borderWidth={2}
+        onLoad={() => setImageLoaded(true)}
+        onLoadStart={() => setImageLoaded(false)}
+        opacity={imageLoaded ? 1 : 0} // Ensure the image becomes visible only when loaded
         {...rest}
       />
     </Center>
