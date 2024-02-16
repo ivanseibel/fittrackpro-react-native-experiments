c0191744ea0c65b35f4864f131c7070ff72cabc7
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-16 20:37:00 +0000

    Add skeleton loading for user photo in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,18 +1,33 @@
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, ScrollView, VStack } from 'native-base'
+import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Profile" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            size={33}
-            alt="Ivan Seibel"
-            source={{ uri: 'https://github.com/ivanseibel.png' }}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              startColor="gray.500"
+              endColor="gray.400"
+              height={PHOTO_SIZE}
+              width={PHOTO_SIZE}
+              rounded={'full'}
+            />
+          ) : (
+            <UserPhoto
+              size={PHOTO_SIZE}
+              alt="Ivan Seibel"
+              source={{ uri: 'https://github.com/ivanseibel.png' }}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
