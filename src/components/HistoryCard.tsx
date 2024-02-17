deceebeee945fc9b82a9a51e05f3462181f687ce
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-17 18:36:31 +0000

    Update HistoryCard component to display group name in a single line

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -17,19 +17,20 @@ export function HistoryCard({ group, name, time }: HistoryCardProps) {
       alignItems={'center'}
       justifyContent={'space-between'}
     >
-      <VStack mr={5}>
+      <VStack mr={5} flex={1}>
         <Heading
           color="white"
           fontSize={'md'}
           lineHeight={'md'}
           textTransform={'capitalize'}
+          numberOfLines={1}
         >
           {group}
         </Heading>
         <Text
           color="gray.100"
           fontSize={'lg'}
-          numberOfLines={2}
+          numberOfLines={1}
           lineHeight={'lg'}
         >
           {name}
