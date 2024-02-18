03873eef54754f2306bca260bbcff729fdf1134e
Ivan Seibel <me@ivanseibel.dev>
2024-02-18 18:11:58 +0000

    Refactor Input component to include error handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,25 +1,36 @@
-import { Input as NativeBaseInput } from 'native-base'
+import { FormControl, Input as NativeBaseInput } from 'native-base'
 
-type InputProps = React.ComponentProps<typeof NativeBaseInput>
+type InputProps = React.ComponentProps<typeof NativeBaseInput> & {
+  errorMessage?: string | null
+}
 
-export function Input(props: InputProps) {
+export function Input({ errorMessage = null, isInvalid, ...rest }: InputProps) {
+  const inputIsInvalid = !!errorMessage || isInvalid
+
   return (
-    <NativeBaseInput
-      bg="gray.650"
-      h={14}
-      px={4}
-      borderWidth={0}
-      fontSize={'md'}
-      color={'white'}
-      fontFamily={'body'}
-      placeholderTextColor={'gray.300'}
-      rounded={'6px'}
-      _focus={{
-        bg: 'gray.650',
-        borderWidth: 1,
-        borderColor: 'green.500',
-      }}
-      {...props}
-    />
+    <FormControl isInvalid={inputIsInvalid}>
+      <FormControl.ErrorMessage m={0}>{errorMessage}</FormControl.ErrorMessage>
+      <NativeBaseInput
+        bg="gray.650"
+        h={14}
+        px={4}
+        borderWidth={0}
+        fontSize={'md'}
+        color={'white'}
+        fontFamily={'body'}
+        placeholderTextColor={'gray.300'}
+        rounded={'6px'}
+        _invalid={{
+          borderColor: 'red.600',
+          borderWidth: 1,
+        }}
+        _focus={{
+          bg: 'gray.650',
+          borderWidth: 1,
+          borderColor: 'green.500',
+        }}
+        {...rest}
+      />
+    </FormControl>
   )
 }
