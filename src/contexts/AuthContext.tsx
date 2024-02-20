b33995b201cf5db623d6e83f2eadee26451f0bba
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-20 12:09:57 +0000

    Add AuthContextProvider component

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,9 +6,31 @@ type AuthContextDataType = {
   isAuthenticated: boolean
 }
 
+type AuthContextProviderProps = {
+  children: React.ReactNode
+}
+
 const INITIAL_STATE: AuthContextDataType = {
   user: {} as UserDTO,
   isAuthenticated: false,
 }
 
 export const AuthContext = createContext<AuthContextDataType>(INITIAL_STATE)
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return (
+    <AuthContext.Provider
+      value={{
+        user: {
+          id: '1',
+          name: 'User',
+          email: 'user@email.com',
+          avatar: 'user.png',
+        },
+        isAuthenticated: false,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
