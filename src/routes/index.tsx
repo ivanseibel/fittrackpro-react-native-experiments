c82775a923ebaa30937a07c3ebf89238c427e04d
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-20 14:10:12 +0000

    Add loading component to Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,10 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { AuthRoutes } from './auth.routes'
 import { AppRoutes } from './app.routes'
 import { useAuth } from '@hooks/useAuth'
+import { Loading } from '@components/Loading'
 
 export function Routes() {
-  const { isAuthenticated } = useAuth()
+  const { user, isLoading } = useAuth()
 
   const { colors } = useTheme()
   const theme = {
@@ -16,10 +17,14 @@ export function Routes() {
     },
   }
 
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        {isAuthenticated ? <AppRoutes /> : <AuthRoutes />}
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
