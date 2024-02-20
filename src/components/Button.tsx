8a49bdd9279dbdd578e7f93a5911dc8c0b2f72ba
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-20 13:35:25 +0000

    Add isLoading prop to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,24 @@ import { IButtonProps, Button as NativeBaseButton } from 'native-base'
 
 type ButtonProps = IButtonProps & {
   variant?: 'solid' | 'outline'
+  // isLoading?: boolean
 }
 
-export function Button({ variant = 'solid', children, ...rest }: ButtonProps) {
+export function Button({
+  variant = 'solid',
+  // isLoading = false,
+  children,
+  ...rest
+}: ButtonProps) {
   return (
     <NativeBaseButton
       rounded={'6px'}
       bg={variant === 'outline' ? 'transparent' : 'green.700'}
       borderWidth={variant === 'outline' ? 1 : 0}
       borderColor={'green.500'}
+      _loading={{
+        bg: variant === 'outline' ? 'green.700' : 'green.500',
+      }}
       _text={{
         fontFamily: 'heading',
         color: variant === 'outline' ? 'green.700' : 'white',
