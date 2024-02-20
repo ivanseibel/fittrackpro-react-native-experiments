90e9ed0bd3af3009bd098ca23dc8f4db06239f48
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-20 00:03:45 +0000

    Add TOAST_DEFAULT constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+import { IToastProps } from 'native-base'
+
+export const TOAST_DEFAULT: IToastProps = {
+  placement: 'top',
+  _description: {
+    fontFamily: 'body',
+    fontSize: 'md',
+  },
+  _title: {
+    fontFamily: 'heading',
+    fontSize: 'lg',
+  },
+}
