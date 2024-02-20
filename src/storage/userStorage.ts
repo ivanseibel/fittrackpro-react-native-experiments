701bdd31f119434e681ee10caa142e7a0a97fa85
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-20 13:58:16 +0000

    Refactor userStorageSave and add userStorageGet and userStorageRemove functions

--- src/storage/userStorage.ts
+++ src/storage/userStorage.ts
@@ -3,8 +3,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { USER_STORAGE_PREFIX } from 'src/utils/constants'
 
 export async function userStorageSave(user: UserDTO) {
-  await AsyncStorage.setItem(
-    `${USER_STORAGE_PREFIX}:${user.id}`,
-    JSON.stringify(user),
-  )
+  await AsyncStorage.setItem(USER_STORAGE_PREFIX, JSON.stringify(user))
+}
+
+export async function userStorageGet(): Promise<UserDTO | null> {
+  const user = await AsyncStorage.getItem(USER_STORAGE_PREFIX)
+
+  if (!user) {
+    return null
+  }
+
+  return JSON.parse(user) as UserDTO
+}
+
+export async function userStorageRemove() {
+  await AsyncStorage.removeItem(USER_STORAGE_PREFIX)
 }
