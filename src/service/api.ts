1ba9f33af2ca931b12d827c25ca15b53bcc4fc35
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-02-22 11:41:33 +0000

    Add token storage import and register intercept token manager in API instance

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,19 +1,49 @@
+import { tokenStorageGet } from '@storage/tokenStorage'
 import { AppError } from '@utils/AppError'
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+
+type SignOut = () => void
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
+}
 
 const api = axios.create({
   baseURL: 'http://10.10.127.117:3333',
-})
+}) as APIInstanceProps
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message))
-    } else {
-      return Promise.reject(error)
-    }
-  },
-)
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    async (responseError) => {
+      if (responseError?.response?.status === 401) {
+        if (
+          ['token.expired', 'token.invalid'].includes(
+            responseError.response.data?.message,
+          )
+        ) {
+          const tokenObject = await tokenStorageGet()
+
+          if (!tokenObject?.refreshToken) {
+            signOut()
+            return Promise.reject(responseError)
+          }
+        }
+
+        signOut()
+      }
+
+      if (responseError.response && responseError.response.data) {
+        return Promise.reject(new AppError(responseError.response.data.message))
+      } else {
+        return Promise.reject(responseError)
+      }
+    },
+  )
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager)
+  }
+}
 
 export { api }
